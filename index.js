4da27806d183b2aa114dcd654f4084da60d684cb
nimo731 <patience.karanja@student.moringaschool.com>
2025-03-10 12:24:45 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase(); 
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+var bestCustomer;
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "leon"
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "leon";
+}
+
+
